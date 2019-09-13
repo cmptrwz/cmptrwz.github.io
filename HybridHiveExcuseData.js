0503532e5bc22a049bde2cf7895f0b2ea2b3efe3
CmptrWz <44703478+cmptrwz@users.noreply.github.com>
2019-09-13 14:52:26 -0400

    Add some basic Hybrid Hive data

---
+++ HybridHiveExcuseData.js
@@ -0,0 +1,14 @@
+// Fandom-specific type
+fantypes["Hybrid Hive"] = {};
+fantypes["Hybrid Hive"].hero_group = [
+	{name:"Mana Bullshit",member:"a member of",supermembers:"members of",simple_name:"Mana",article:"a"},
+];
+fantypes["Hybrid Hive"].hero = [
+	{name:"Minerva",article:"a",possessive:"her",item:["halberd","cutting tool","lightsaber"],itemarticle:["a halberd","a cutting tool","a lightsaber"],group:fantypes["Hybrid Hive"].hero_group[0]},
+	{name:"Lilia",article:"a",possessive:"her",item:["halberd","cutting tool","lightsaber"],itemarticle:["a halberd","a cutting tool","a lightsaber"],group:fantypes["Hybrid Hive"].hero_group[0]},
+];
+fantypes["Hybrid Hive"].swingthing = [
+	{name:"halberd",article:"a"},
+	{name:"cutting tool",article:"a"},
+	{name:"lightsaber",article:"a"},
+];
